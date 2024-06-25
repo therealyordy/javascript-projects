7d4a5439d1c37323f90b7b3b2966a9ce36c4b561
Yordy Garrido Martin <116683087+therealyordy@users.noreply.github.com>
2024-06-25 16:07:32 -0500

    while-loop

--- loops/exercises/for-Loop-Exercises.js
+++ loops/exercises/for-Loop-Exercises.js
@@ -3,8 +3,33 @@
     b. Print only the ODD values from 3 - 29, one number per line.
     c. Print the EVEN numbers 12 to -14 in descending order, one number per line.
     d. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers). */
+  for (let i = 0; i <= 20; i++) {
+      console.log(i);
+   }
+
+   console.log("\nTask 2:");
+
+   for (let i = 3; i <= -29; i += 2) {
+    console.log(i);
+}
+
+console.log("Task 1:");
+for (let i = 0; i <= 20; i++) {
+    console.log(i);
+}
 
 
+console.log("\nTask 3:");
+for (let i = 12; i >= -14; i -= 2) {
+    console.log(i);
+}
+
+console.log("\nTask 4:");
+for (let i = 50; i >= 20; i--) {
+    if (i % 3 === 0) {
+        console.log(i);
+    }
+}
 
 
 /*Exercise #2: 
@@ -15,8 +40,15 @@ Construct ``for`` loops to accomplish the following tasks:
   a. Print each element of the array to a new line.
   b. Print each character of the string - in reverse order - to a new line. */ 
 
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+ }
 
+ let otherArr = [2, 3, 13, 18, -5, 38, -10, 11, 0, 104];
+let evens = [], odds = [];
 
+console.log(evens);
+console.log(odds);
 
 
 /*Exercise #3:Construct a for loop that sorts the array [2, 3, 13, 18, -5, 38, -10, 11, 0, 104] into two new arrays:
--- loops/exercises/while-Loop-Exercises.js
+++ loops/exercises/while-Loop-Exercises.js
@@ -1,6 +1,11 @@
 //Define three variables for the LaunchCode shuttle - one for the starting fuel level, another for the number of astronauts aboard, and the third for the altitude the shuttle reaches.
 
+const input = require('readline-sync');
+let fuelLevel = 0, numAstronauts = 0, altitude = 0;
 
+while (fuelLevel <= 5000 || fuelLevel > 30000 || isNaN(fuelLevel)) {
+   fuelLevel = input.question("Enter the starting fuel level: ");
+}
 
 
 
@@ -23,3 +28,8 @@
 /*Exercise #5: Output the result with the phrase, “The shuttle gained an altitude of ___ km.”
 
 If the altitude is 2000 km or higher, add “Orbit achieved!” Otherwise add, “Failed to reach orbit.”*/
+let output = `The shuttle gained an altitude of ${altitude} km.`;
+
+if (altitude >= 2000) {
+output += " Orbit achieved!";
+}
